3958f7455645a1fc411d05dfbbcb1fbc78bdcd58
LILMCJOE <songajobs@gmail.com>
2024-09-15 01:51:59 +0200

    0. Initial Setup and Project Configuration for a Django Blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -84,6 +84,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+        'USER': 'songa_joel',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
