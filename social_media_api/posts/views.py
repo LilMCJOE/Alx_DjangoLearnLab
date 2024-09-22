8c7a8b46065ceb01dee8bad9839bb4843bfb02ec
LILMCJOE <songajobs@gmail.com>
2024-09-22 20:37:46 +0200

    3. Implementing Notifications and Likes Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
@@ -10,7 +10,7 @@ from rest_framework.response import Response
 from .models import Post, Like
 from accounts.models import CustomUser
 from notifications.models import Notification
-
+from django.shortcuts import get_object_or_404
 class PostPagination(PageNumberPagination):
     page_size = 10
 
